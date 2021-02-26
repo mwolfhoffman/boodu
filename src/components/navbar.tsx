06a664f25015cf15f1dab62d6269b4324f053a48
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-02-25 21:17:29 -0700

    some styling

---
+++ src/components/navbar.tsx
@@ -0,0 +1,22 @@
+export default function () {
+  return (
+    <>
+      <ul>
+        <li>
+          <a className="active" href="#home">
+            Dashboard
+          </a>
+        </li>
+        <li>
+          <a href="#news">Organizaions</a>
+        </li>
+        <li>
+          <a href="#contact">Settings</a>
+        </li>
+        <li>
+            <a href="#"> Logout </a>
+        </li>
+      </ul>
+    </>
+  );
+}
--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -1,6 +1,8 @@
+import "../assets/styles/navbar.scss";
 import { useHistory } from "react-router-dom";
-import {useEffect } from "react";
+import { useEffect } from "react";
 import supabase from "../supabase";
+import Navbar from "../components/navbar";
 
 export default function () {
   let history = useHistory();
@@ -8,7 +10,7 @@ export default function () {
   useEffect(() => {
     const checkUser = async () => {
       const user = await supabase.auth.user();
-      debugger
+      console.log(user);
       if (!user) {
         history.push("/login");
       }
@@ -16,5 +18,10 @@ export default function () {
     checkUser();
   }, []);
 
-  return <>Dashboard</>;
+  return (
+    <>
+      <Navbar />
+      <h1>Dashboard</h1>
+    </>
+  );
 }
