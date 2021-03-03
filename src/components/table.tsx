208ea54b1198d212880c169b7fe5c9b478082290
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-03-03 11:26:52 -0700

    creating columns on table page

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -5,6 +5,8 @@ import { useParams } from "react-router-dom";
 export default function (props) {
   let { id }: any = useParams();
   let [tableItems, setTableItems] = useState([]);
+  let [newColumnName, setNewColumnName] = useState("");
+  let [newColumnType, setNewColumnType] = useState("Bool");
 
   const fetchTableItems = async () => {
     const { data, error } = await supabase
@@ -22,6 +24,25 @@ export default function (props) {
     fetchTableItems();
   }, [id]);
 
+  const createNewColumn = async (e: any) => {
+    e.preventDefault();
+    console.log(newColumnName, newColumnType);
+    const { data, error } = await supabase.from("table_item").insert({
+      table_id: props.table.id,
+      name: newColumnName,
+      type: newColumnType,
+    });
+    if (data) {
+      await fetchTableItems();
+      setNewColumnName((n) => (n = ""));
+      setNewColumnType((t) => (t = "Bool"));
+    }
+  };
+
+  const setColumnTypeOption = (value: string) => {
+    setNewColumnType((t) => (t = value));
+  };
+
   return (
     <>
       <h3>{props.table.name}</h3>
@@ -30,15 +51,43 @@ export default function (props) {
         <div>
           {tableItems.map((c: any) => {
             return (
-              <>
+              <div key={c.id}>
                 {c.name} : {c.type}
-              </>
+              </div>
             );
           })}
         </div>
       ) : (
         <p>You have no columns in this table yet.</p>
       )}
+
+      <div>
+        <form onSubmit={(event: any) => createNewColumn(event)}>
+          <input
+            type="text"
+            placeholder="Column Name"
+            value={newColumnName}
+            onChange={(e: any) => {
+              setNewColumnName((c) => (c = e?.target?.value));
+            }}
+          />
+          <select
+            name="data-type-select"
+            id="data-type-select"
+            value={newColumnType}
+            onChange={(e) => setColumnTypeOption(e.target.value)}
+          >
+            <option value="Bool">Bool</option>
+            <option value="Date">Date</option>
+            <option value="Int4">Int4</option>
+            <option value="Int8">Int8</option>
+            <option value="Text">Text</option>
+            <option value="Time">Time</option>
+            <option value="Timestamp">Timestamp</option>
+          </select>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>
   );
 }
