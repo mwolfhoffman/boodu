5333a49fb32bbedfc10901a9b49d9b2e9b485e3b
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-03-03 12:05:27 -0700

    tailwind started

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // removes unused styles in production.
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
