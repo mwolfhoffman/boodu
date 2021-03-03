acd8f6913cf046c5c741b2ce4bb48a47287f7db7
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-03-02 19:32:16 -0700

    added GH action CI 2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: any = null;
+
+describe("App.js", () => {
+  beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("Component can be created.", () => {
+    act(function () {
+      render(<App />, container);
+    });
+    expect(container).toBeTruthy();
+  });
 });
