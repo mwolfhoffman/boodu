ac5ddc5f7358f116a6c8e480b191a371d6aa209d
Michael Hoffman <michaelhoffmanj@gmail.com>
2021-03-04 12:01:30 -0700

    added back ability to create column on tables view

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -2,6 +2,7 @@ import "../assets/styles/table.scss";
 import supabase from "../supabase";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { table } from "console";
 
 export default function (props) {
   let { id }: any = useParams();
@@ -40,54 +41,70 @@ export default function (props) {
   const setColumnTypeOption = (value: string) => {
     setNewColumnType((t) => (t = value));
   };
+  const deleteColumn = async (e: any, id: string) => {
+    e.preventDefault();
 
+    const { data, error } = await supabase
+      .from("table_item")
+      .delete()
+      .match({ id: id });
+    if (data) {
+      debugger;
+      await fetchTableItems();
+    }
+  };
   return (
     <>
       <h2>{props.table.name}</h2>
 
       <table>
-        {tableItems?.length ? (
-          <>
-            {tableItems?.map((t) => {
-              return (
-                <tr>
-                  <td>{t.name}</td>
-                  <td>{t.type}</td>
-                  <td></td>
-                </tr>
-              );
-            })}
-          </>
-        ) : <p>No columns yet...</p>}
+        {tableItems?.map((t) => {
+          return (
+            <tr>
+              <td>{t.name}</td>
+              <td>{t.type}</td>
+              <td>
+                <button onClick={(event: any) => deleteColumn(event, t.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+        <tr>
+          <td>
+            <input
+              type="text"
+              placeholder="New Column Name"
+              value={newColumnName}
+              onChange={(e: any) => {
+                setNewColumnName((n) => (n = e.target.value));
+              }}
+            />
+          </td>
+          <td>
+            <select
+              name="data-type-select"
+              id="data-type-select"
+              value={newColumnType}
+              onChange={(e) => setColumnTypeOption(e.target.value)}
+            >
+              <option value="Bool">Bool</option>
+              <option value="Date">Date</option>
+              <option value="Int4">Int4</option>
+              <option value="Int8">Int8</option>
+              <option value="Text">Text</option>
+              <option value="Time">Time</option>
+              <option value="Timestamp">Timestamp</option>
+            </select>
+          </td>
+          <td>
+            <button onClick={(event: any) => createNewColumn(event)}>
+              Add
+            </button>
+          </td>
+        </tr>
       </table>
-
-      {/* <div>
-        <form onSubmit={(event: any) => createNewColumn(event)}>
-          <input
-            type="text"
-            placeholder="Column Name"
-            value={newColumnName}
-            onChange={(e: any) => {
-              setNewColumnName((c) => (c = e?.target?.value));
-            }}
-          />
-          <select
-            name="data-type-select"
-            id="data-type-select"
-            value={newColumnType}
-            onChange={(e) => setColumnTypeOption(e.target.value)}
-          >
-            <option value="Bool">Bool</option>
-            <option value="Date">Date</option>
-            <option value="Int4">Int4</option>
-            <option value="Int8">Int8</option>
-            <option value="Text">Text</option>
-            <option value="Time">Time</option>
-            <option value="Timestamp">Timestamp</option>
-          </select>
-          <button type="submit">Submit</button>
-        </form>
-      </div> */}
     </>
   );
 }
